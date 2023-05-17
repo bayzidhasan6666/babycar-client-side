3b91edad9e609ae8adeaf2f91fdb31905408bd27
bayzidhasan6666 <121633867+bayzidhasan6666@users.noreply.github.com>
2023-05-17 20:44:50 +0800

    git initialization

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>Hello From app ...assignment 11 is coming soon...</div>;
+};
+
+export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDpQRJb9-TjZJoqHy-y1Ivd1yUvjeQnkMc',
+  authDomain: 'assignment-11-9fde9.firebaseapp.com',
+  projectId: 'assignment-11-9fde9',
+  storageBucket: 'assignment-11-9fde9.appspot.com',
+  messagingSenderId: '336047911485',
+  appId: '1:336047911485:web:11136b2c4f9c94d5c67054',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
