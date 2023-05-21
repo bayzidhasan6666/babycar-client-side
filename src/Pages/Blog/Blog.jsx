53ea21ff7530f0bd946147ab8b215563dd866a2d
bayzidhasan6666 <121633867+bayzidhasan6666@users.noreply.github.com>
2023-05-21 15:42:35 +0800

    blog updated

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,148 +1,109 @@
-import React, { useState } from 'react';
-import { FaDatabase, FaCode, FaCogs } from 'react-icons/fa';
-import useTitle from '../../PageTitle/useTitle';
+import React, { useState, useEffect } from 'react';
+import { FaAcquisitionsIncorporated, FaQuestion } from 'react-icons/fa';
+import { FiDatabase } from 'react-icons/fi';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Blog = () => {
-  useTitle('Blog Page');
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-  const [pinnedBlogs, setPinnedBlogs] = useState([]);
+  const faqData = [
+    {
+      question: 'What is an access token and refresh token',
+      answer:
+        "Access tokens and refresh tokens are commonly used in authentication systems. An access token is a credential that is issued to a user after they successfully authenticate themselves. It contains information about the user's identity and permissions. Access tokens are usually short-lived and have an expiration time.",
+      icon: <FaQuestion />,
+      database: 'Authentication',
+    },
+    {
+      question: 'Compare SQL and NoSQL databases',
+      answer:
+        'SQL and NoSQL databases differ in their data models, querying languages, and scalability. SQL databases use structured query language and have a predefined schema. They are best suited for complex queries and strict consistency requirements. NoSQL databases are schema-less and provide flexible data models. They are suitable for large-scale applications with high write loads and need for horizontal scaling.',
+      icon: <FaQuestion />,
+      database: 'Databases',
+    },
+    {
+      question: 'What is Express.js',
+      answer:
+        'Express.js is a fast and minimalist web application framework for Node.js. It provides a robust set of features for web and mobile application development, including routing, middleware support, and template engine integration. Express.js allows developers to build scalable and modular web applications with ease.',
+      icon: <FaQuestion />,
+      database: 'Web Development',
+    },
+    {
+      question: 'What is Nest.js',
+      answer:
+        'Nest.js is a progressive Node.js framework for building efficient, scalable, and maintainable server-side applications. It is built with TypeScript and takes advantage of its features like decorators, strong typing, and dependency injection. Nest.js follows the architectural style of Angular and provides a solid foundation for building enterprise-grade applications.',
+      icon: <FaQuestion />,
+      database: 'Web Development',
+    },
+    {
+      question: 'What is MongoDB aggregate and how does it work',
+      answer:
+        'MongoDB aggregate is a powerful data processing operation used for performing complex data analysis tasks on MongoDB collections. It allows you to process and transform data using various stages like filtering, grouping, sorting, and aggregating. Aggregation pipelines in MongoDB provide a flexible way to perform data aggregation operations and generate insightful results.',
+      icon: <FaQuestion />,
+      database: 'Databases',
+    },
+  ];
 
-  const handlePinToggle = (blog) => {
-    if (isPinned(blog)) {
-      unpinBlog(blog);
-    } else {
-      pinBlog(blog);
-    }
-  };
+  const [activeIndex, setActiveIndex] = useState(null);
 
-  const isPinned = (blog) => {
-    return pinnedBlogs.some((pinnedBlog) => pinnedBlog === blog);
-  };
-
-  const pinBlog = (blog) => {
-    setPinnedBlogs([...pinnedBlogs, blog]);
-  };
-
-  const unpinBlog = (blog) => {
-    setPinnedBlogs(pinnedBlogs.filter((pinnedBlog) => pinnedBlog !== blog));
+  const toggleAccordion = (index) => {
+    setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
   };
 
   return (
-    <div className="container mx-auto p-8">
-      <div className="flex items-center mb-5 justify-center space-x-4">
-        <FaDatabase className="text-4xl text-blue-500" />
-        <FaCode className="text-4xl text-purple-500" />
-        <FaCogs className="text-4xl text-green-500" />
+    <section className="">
+      <h2
+        data-aos="fade-right"
+        className="text-2xl flex items-center mx-auto w-fit text-center text-purple-500 font-semibold mb-4"
+      >
+        Assignment Required Questions <FaQuestion></FaQuestion>
+      </h2>
+      <div className="container mx-auto">
+        {faqData.map((faq, index) => (
+          <div
+            key={index}
+            data-aos="fade-left"
+            data-aos-delay={index * 100}
+            className="border border-purple-600 rounded p-6 mb-6"
+          >
+            <div
+              className="flex justify-between items-center cursor-pointer"
+              onClick={() => toggleAccordion(index)}
+            >
+              <h3 className="text-xl text-purple-500 items-center flex font-semibold mb-2">
+                {faq.question}
+                {faq.icon}
+              </h3>
+              <button
+                className={`btn border   ${
+                  activeIndex === index
+                    ? 'text-teal-500 border-teal-500'
+                    : 'text-purple-500 border-purple-500'
+                }`}
+              >
+                {activeIndex === index ? (
+                  <FiDatabase className="text-teal-500" />
+                ) : (
+                  <FiDatabase className="text-purple-500" />
+                )}
+              </button>
+            </div>
+            {activeIndex === index && (
+              <div className="text-teal-500 mb-4">
+                <p>{faq.answer}</p>
+              </div>
+            )}
+            <div className="flex items-center text-teal-500">
+              <FiDatabase className="mr-1 text-purple-500" />
+              <span>{faq.database}</span>
+            </div>
+          </div>
+        ))}
       </div>
-      <div className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">
-          Access Token and Refresh Token
-        </h2>
-        <p>
-          An access token is a credential used to authenticate and authorize
-          access to protected resources. It is usually a JSON web token (JWT)
-          that contains information about the user and their permissions. Access
-          tokens have a limited lifespan and are used to make authorized API
-          requests.
-        </p>
-        <p>
-          A refresh token is a longer-lived token used to obtain a new access
-          token without requiring the user to log in again. When an access token
-          expires, the refresh token can be used to obtain a new access token.
-          Refresh tokens are securely stored and should be kept confidential.
-        </p>
-        <p>
-          On the client-side, access tokens are typically stored in memory or
-          browser storage (e.g., local storage or session storage). Refresh
-          tokens, on the other hand, should be securely stored in an HTTP-only
-          cookie to protect against cross-site scripting (XSS) attacks.
-        </p>
-      </div>
-
-      <div className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">SQL vs NoSQL Databases</h2>
-        <p>
-          SQL (Structured Query Language) databases are relational databases
-          that store data in tables with predefined schemas. They use SQL for
-          querying and manipulating data. SQL databases provide strong
-          consistency, support complex relationships between entities, and are
-          suitable for structured data with strict integrity requirements.
-        </p>
-        <p>
-          NoSQL (Not Only SQL) databases, on the other hand, are non-relational
-          databases that store data in flexible schemas like key-value pairs,
-          documents, or graphs. They provide scalability, high availability, and
-          flexible data models. NoSQL databases are suitable for handling
-          unstructured or semi-structured data, and they often prioritize high
-          performance and horizontal scalability.
-        </p>
-      </div>
-
-      <div className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">Express.js and Nest.js</h2>
-        <p>
-          Express.js is a popular web application framework for Node.js. It
-          provides a minimalist and flexible approach to building web servers
-          and APIs. Express.js allows you to handle routes, middleware, and HTTP
-          requests easily, making it a great choice for building RESTful APIs
-          and web applications.
-        </p>
-        <p>
-          Nest.js is a progressive TypeScript framework for building scalable
-          and maintainable server-side applications. It is built on top of
-          Express.js and provides additional features and architectural patterns
-          inspired by Angular. Nest.js promotes the use of decorators, modules,
-          and dependency injection to create structured and extensible
-          applications.
-        </p>
-      </div>
-
-      <div>
-        <h2 className="text-2xl font-bold mb-4">MongoDB Aggregate</h2>
-        <p>
-          MongoDB Aggregate is a framework within MongoDB that allows you to
-          perform advanced data processing and analysis operations on the data
-          stored in MongoDB collections. It provides a way to perform complex
-          aggregations, transformations, and computations on the data using a
-          pipeline of stages.
-        </p>
-        <p>
-          The aggregation pipeline consists of multiple stages, such as
-          filtering, grouping, sorting, and projecting data. Each stage takes
-          input from the previous stage and passes the transformed data to the
-          next stage. Aggregation operations can include various operators and
-          expressions to manipulate and process the data.
-        </p>
-        <p>
-          MongoDB Aggregate is a powerful tool for performing analytical
-          queries, generating reports, and obtaining insights from your MongoDB
-          data.
-        </p>
-      </div>
-
-      <div className="flex items-center justify-center mt-8">
-        <div className="flex items-center space-x-4">
-          <FaDatabase
-            className={`text-4xl ${
-              isPinned('access-token') ? 'text-blue-800' : 'text-blue-500'
-            }`}
-            onClick={() => handlePinToggle('access-token')}
-          />
-          <FaCode
-            className={`text-4xl ${
-              isPinned('sql-nosql') ? 'text-purple-800' : 'text-purple-500'
-            }`}
-            onClick={() => handlePinToggle('sql-nosql')}
-          />
-          <FaCogs
-            className={`text-4xl ${
-              isPinned('express-nest') ? 'text-green-800' : 'text-green-500'
-            }`}
-            onClick={() => handlePinToggle('express-nest')}
-          />
-        </div>
-      </div>
-    </div>
+    </section>
   );
 };
 
