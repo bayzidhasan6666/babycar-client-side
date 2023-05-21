e39489ff816b4dfe1b6d1771b1a543e35fe92e6a
bayzidhasan6666 <121633867+bayzidhasan6666@users.noreply.github.com>
2023-05-21 20:03:59 +0800

    banner responsive

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -25,11 +25,11 @@ const Banner = () => {
         <div>
           <div className="carousel-item">
             <img
-              className="h-96 w-full lg:h-[550px] "
+              className="h-96  md:w-full lg:h-[550px] "
               src="https://files.vanceai.com/api/v2/preview?web=vanceai&guest_token_v2=af16e142360b10201aadab263248d4aa&trans_id=e0b5a735f22aff7c14abaa064a9a52bd"
               alt="Banner 1"
             />
-            <div className="carousel-item-content ml-10 top-20 absolute space-y-3 w-96 mx-auto">
+            <div className="carousel-item-content ml-10 top-20 absolute space-y-3 w-64 md:w-96 mx-auto">
               <h2 className="font-bold text-4xl text-purple-600">
                 Explore Our Baby Car Collection
               </h2>
@@ -51,11 +51,11 @@ const Banner = () => {
         <div>
           <div className="carousel-item">
             <img
-              className="h-96 w-full lg:h-[550px]"
+              className="h-96  md:w-full lg:h-[550px]"
               src="https://files.vanceai.com/api/v2/preview?web=vanceai&guest_token_v2=af16e142360b10201aadab263248d4aa&trans_id=476f2e07394c0c9e2eaa84831acec704"
               alt="Banner 2"
             />
-            <div className="carousel-item-content ml-10 top-20 absolute space-y-3 w-96 mx-auto">
+            <div className="carousel-item-content ml-10 top-20 absolute space-y-3 w-64 md:w-96 mx-auto">
               <h2 className="font-bold text-4xl text-purple-600">
                 Discover Fun and Safe Baby Cars
               </h2>
@@ -77,11 +77,11 @@ const Banner = () => {
         <div className="">
           <div className="carousel-item">
             <img
-              className="h-96 w-full lg:h-[550px]"
+              className="h-96  md:w-full lg:h-[550px]"
               src="https://images.unsplash.com/photo-1590504263777-ee53135bdbdc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1041&q=80"
               alt="Banner 3"
             />
-            <div className="carousel-item-content ml-10 top-20 absolute space-y-3 w-96 mx-auto">
+            <div className="carousel-item-content ml-10 top-20 absolute space-y-3 w-64 md:w-96 mx-auto">
               <h2 className="font-bold text-4xl text-purple-600">
                 Quality Baby Cars for Unforgettable Adventures
               </h2>
